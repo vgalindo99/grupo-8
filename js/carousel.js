805ef3b97a8e8815e4e094bb8f626e4b23bb97e9
Victor <you@example.com>
2019-09-02 20:03:04 -0500

    semanas

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,12 @@
-const item1 = document.getElementById('item1');
-const item2 = document.getElementById('item2');
-const item3 = document.getElementById('item3');
-const item4 = document.getElementById('item4');
-const item5 = document.getElementById('item5');
-const arrowLeft = document.getElementById('arrow-left');
-const arrowRight = document.getElementById('arrow-right');
-const semanas = document.getElementById('semanas');
+const item1 = document.getElementById("item1");
+const item2 = document.getElementById("item2");
+const item3 = document.getElementById("item3");
+const item4 = document.getElementById("item4");
+const item5 = document.getElementById("item5");
+const arrowLeft = document.getElementById("arrow-left");
+const arrowRight = document.getElementById("arrow-right");
+const semanas = document.getElementById("semanas");
+
 let a = 0;
 item1.style.left = `${26.25 * 0}rem`;
 item2.style.left = `${26.25 * 1}rem`;
@@ -14,103 +15,97 @@ item4.style.left = `${26.25 * 3}rem`;
 item5.style.left = `${26.25 * 4}rem`;
 
 async function move(e, ini, fin) {
-	a = 1;
-	await e.animate(
-		[
-			{ left: ini },
-			{ left: fin }
-		],
-		{
-			duration : 500,
-			easing   : 'ease'
-		}
-	);
-	e.style.left = fin;
-	setTimeout(() => {
-		a = 0;
-	}, 500);
+  a = 1;
+  await e.animate([{ left: ini }, { left: fin }], {
+    duration: 500,
+    easing: "ease"
+  });
+  e.style.left = fin;
+  setTimeout(() => {
+    a = 0;
+  }, 500);
 }
 
 function movementLeft(e) {
-	if (e.style.left == `${26.25 * 4}rem` || e.style.left == `${-26.25 * 1}rem`) {
-		move(e, `${26.25 * -1}rem`, `${26.25 * 0}rem`);
-	} else if (e.style.left == `${26.25 * 0}rem`) {
-		move(e, `${26.25 * 0}rem`, `${26.25 * 1}rem`);
-	} else if (e.style.left == `${26.25 * 1}rem`) {
-		move(e, `${26.25 * 1}rem`, `${26.25 * 2}rem`);
-	} else if (e.style.left == `${26.25 * 2}rem`) {
-		move(e, `${26.25 * 2}rem`, `${26.25 * 3}rem`);
-	} else if (e.style.left == `${26.25 * 3}rem`) {
-		move(e, `${26.25 * 3}rem`, `${26.25 * 4}rem`);
-	}
+  if (e.style.left == `${26.25 * 4}rem` || e.style.left == `${-26.25 * 1}rem`) {
+    move(e, `${26.25 * -1}rem`, `${26.25 * 0}rem`);
+  } else if (e.style.left == `${26.25 * 0}rem`) {
+    move(e, `${26.25 * 0}rem`, `${26.25 * 1}rem`);
+  } else if (e.style.left == `${26.25 * 1}rem`) {
+    move(e, `${26.25 * 1}rem`, `${26.25 * 2}rem`);
+  } else if (e.style.left == `${26.25 * 2}rem`) {
+    move(e, `${26.25 * 2}rem`, `${26.25 * 3}rem`);
+  } else if (e.style.left == `${26.25 * 3}rem`) {
+    move(e, `${26.25 * 3}rem`, `${26.25 * 4}rem`);
+  }
 }
 function movementRight(e) {
-	if (e.style.left == `${26.25 * 4}rem` || e.style.left == `${26.25 * -1}rem`) {
-		move(e, `${26.25 * 4}rem`, `${26.25 * 3}rem`);
-	} else if (e.style.left == `${26.25 * 3}rem`) {
-		move(e, `${26.25 * 3}rem`, `${26.25 * 2}rem`);
-	} else if (e.style.left == `${26.25 * 2}rem`) {
-		move(e, `${26.25 * 2}rem`, `${26.25 * 1}rem`);
-	} else if (e.style.left == `${26.25 * 1}rem`) {
-		move(e, `${26.25 * 1}rem`, `${26.25 * 0}rem`);
-	} else if (e.style.left == `${26.25 * 0}rem`) {
-		move(e, `${26.25 * 0}rem`, `${26.25 * -1}rem`);
-	}
+  if (e.style.left == `${26.25 * 4}rem` || e.style.left == `${26.25 * -1}rem`) {
+    move(e, `${26.25 * 4}rem`, `${26.25 * 3}rem`);
+  } else if (e.style.left == `${26.25 * 3}rem`) {
+    move(e, `${26.25 * 3}rem`, `${26.25 * 2}rem`);
+  } else if (e.style.left == `${26.25 * 2}rem`) {
+    move(e, `${26.25 * 2}rem`, `${26.25 * 1}rem`);
+  } else if (e.style.left == `${26.25 * 1}rem`) {
+    move(e, `${26.25 * 1}rem`, `${26.25 * 0}rem`);
+  } else if (e.style.left == `${26.25 * 0}rem`) {
+    move(e, `${26.25 * 0}rem`, `${26.25 * -1}rem`);
+  }
 }
 
-arrowLeft.addEventListener('click', () => {
-	if (a === 0) {
-		movementLeft(item1);
-		movementLeft(item2);
-		movementLeft(item3);
-		movementLeft(item4);
-		movementLeft(item5);
-	}
+arrowLeft.addEventListener("click", () => {
+  if (a === 0) {
+    movementLeft(item1);
+    movementLeft(item2);
+    movementLeft(item3);
+    movementLeft(item4);
+    movementLeft(item5);
+  }
 });
-arrowRight.addEventListener('click', () => {
-	if (a === 0) {
-		movementRight(item1);
-		movementRight(item2);
-		movementRight(item3);
-		movementRight(item4);
-		movementRight(item5);
-	}
+arrowRight.addEventListener("click", () => {
+  if (a === 0) {
+    movementRight(item1);
+    movementRight(item2);
+    movementRight(item3);
+    movementRight(item4);
+    movementRight(item5);
+  }
 });
 semanas.style.transform = `translateX(-${0}px)`;
-let count = 0;
-let active = 0;
-let previous = 0;
-let actual = 0;
-function slide(e) {
-	if (e.movementX > 0) {
-		if (actual > 0 || actual + e.movementX > 0) {
-			actual = 0;
-		} else if (actual !== 0) {
-			actual += e.movementX;
-		}
-	}
-	if (e.movementX < 0) {
-		if (actual < -4700 || actual + e.movementX < -4700) {
-			actual = -4700;
-		} else if (actual !== -4700) {
-			actual += e.movementX;
-		}
-	}
-	count++;
-	semanas.style.transform = `translateX(${actual}px)`;
-}
-let slider = (e) => {
-	slide(e);
-};
+// let count = 0;
+// let active = 0;
+// let previous = 0;
+// let actual = 0;
+// function slide(e) {
+// 	if (e.movementX > 0) {
+// 		if (actual > 0 || actual + e.movementX > 0) {
+// 			actual = 0;
+// 		} else if (actual !== 0) {
+// 			actual += e.movementX;
+// 		}
+// 	}
+// 	if (e.movementX < 0) {
+// 		if (actual < -4700 || actual + e.movementX < -4700) {
+// 			actual = -4700;
+// 		} else if (actual !== -4700) {
+// 			actual += e.movementX;
+// 		}
+// 	}
+// 	count++;
+// 	semanas.style.transform = `translateX(${actual}px)`;
+// }
+// let slider = (e) => {
+// 	slide(e);
+// };
 
-semanas.addEventListener('mousedown', (e) => {
-	active = 0;
-	previous = actual;
-	if (e.buttons === 1) {
-		semanas.addEventListener('mousemove', slider);
-	}
-});
-window.addEventListener('mouseup', () => {
-	// For all major browsers, esemanascept IE 8 and earlier
-	semanas.removeEventListener('mousemove', slider);
-});
+// semanas.addEventListener('mousedown', (e) => {
+// 	active = 0;
+// 	previous = actual;
+// 	if (e.buttons === 1) {
+// 		semanas.addEventListener('mousemove', slider);
+// 	}
+// });
+// window.addEventListener('mouseup', () => {
+// 	// For all major browsers, esemanascept IE 8 and earlier
+// 	semanas.removeEventListener('mousemove', slider);
+// });
---
+++ js/semanas.js
@@ -0,0 +1,44 @@
+const show1 = document.getElementById("show1");
+const show2 = document.getElementById("show2");
+const show3 = document.getElementById("show3");
+const show4 = document.getElementById("show4");
+const show5 = document.getElementById("show5");
+const show6 = document.getElementById("show6");
+const show7 = document.getElementById("show7");
+const show8 = document.getElementById("show8");
+const show9 = document.getElementById("show9");
+const show10 = document.getElementById("show10");
+const show11 = document.getElementById("show11");
+const show12 = document.getElementById("show12");
+const show13 = document.getElementById("show13");
+const semanasSelector = document.getElementById("semanas-selector");
+const semanaNumero = document.getElementById("semana-numero");
+
+var i;
+var selector = 1;
+
+function shower(elemento, posicion) {
+  elemento.addEventListener("click", () => {
+    if (semanaNumero.childNodes[0]) {
+      console.log(semanaNumero.childNodes[0]);
+      semanaNumero.removeChild(semanaNumero.childNodes[0]);
+    }
+    var number = document.createTextNode(`${posicion}`);
+    var node = document.createElement("span");
+    node.appendChild(number);
+    semanaNumero.appendChild(node);
+    elemento.style.background = "grey";
+    elemento.style.transitionDelay = "0.4s";
+    semanasSelector.style.left = `${(90 / 12) * (posicion - 1) -
+      0.75 -
+      0.125 * (posicion - 3)}rem`;
+    window[`show${selector}`].style.background = "white";
+    window[`show${selector}`].style.transitionDelay = "0s";
+    window[`show${selector}`].style.transitionDuration = ".1s";
+    selector = posicion;
+  });
+}
+
+for (i = 1; i < 14; i += 1) {
+  shower(window[`show${i}`], i);
+}
